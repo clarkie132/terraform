8732eaad970237637034e5fccbe4506c83c4bd97
David Clark <clarkie132@yahoo.com>
2021-10-15 16:33:45 +0100

    added load balancer

---
+++ load_balancer.tf
@@ -0,0 +1,91 @@
+resource "aws_lb" "example" {
+    name = "terraform-asg-example"
+    load_balancer_type = "application"
+    subnets = data.aws_subnet_ids.default.ids
+    security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+
+    # Allow inbound http requests
+    ingress  {
+      cidr_blocks = [ "0.0.0.0/0" ]
+      from_port = 80
+      protocol = "tcp"
+      to_port = 80
+    } 
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+}
+
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+
+    health_check {
+      path = "/"
+      protocol = "HTTP"
+      matcher = "200"
+      interval = 15
+      healthy_threshold = 2
+      unhealthy_threshold = 2
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+        values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
--- main.tf
+++ main.tf
@@ -2,10 +2,17 @@ provider "aws" {
     region="us-east-2"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -13,8 +20,8 @@ resource "aws_instance" "example" {
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
-    tags = {
-        Name="terraform-example"
+    lifecycle {
+      create_before_destroy = true
     }
 }
 
@@ -27,4 +34,10 @@ resource "aws_security_group" "instance" {
       protocol = "tcp"
       cidr_blocks = [ "0.0.0.0/0" ]
     } 
+}
+
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "the domain of the load balancer"
 }
